303e5119a58bb0ebe09d781e312119756c649c2a
Imam ariadi <gamerapin139@gmail.com>
2025-01-30 22:49:37 +0700

    dome

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+def convert_csv_to_excel(csv_file, excel_file):
+    # Membaca file CSV
+    df = pd.read_csv(csv_file, names=["review", "actual", "predicted"], header=None)
+    
+    # Menyimpan ke file Excel
+    df.to_excel(excel_file, index=False)
+    print(f"File berhasil dikonversi ke {excel_file}")
+
+# Contoh penggunaan
+convert_csv_to_excel("naive_bayes_results.csv", "naive_bayes_results.xlsx")
